db20f3a2cdf83f90675be554c2a8040e1805266c
Andr Zakhara <art.pruttov@gmail.com>
2018-10-25 17:37:46 +0300

    change userFormAction, fix axios

--- src/actions/userFormActions.js
+++ src/actions/userFormActions.js
@@ -25,7 +25,7 @@ export function handleLogin(user) {
             console.log(response);
             dispatch({
                 type: LOGIN_SUCCESS,
-                payload: username,
+                payload: response.body.token,
             })
         })
         .catch(function (error) {

5465964240fd48daa1501c0ee00b3f7576a26adf
AndrZakhara <art.pruttov@gmail.com>
2018-11-08 11:46:10 +0200

    create base app structure

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import { loginFormReducer } from './loginForm';
-
-
-export const rootReducer = combineReducers({
-  loginForm: loginFormReducer,
-  form: formReducer,
-});

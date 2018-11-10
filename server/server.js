d336fb72d6d8f2abf000ca8eb58b1ceff779ba10
AndrZakhara <art.pruttov@gmail.com>
2018-11-10 16:57:09 +0200

    add updates and delete book requests

--- server/server.js
+++ server/server.js
@@ -16,6 +16,17 @@ mongoose.set('useCreateIndex', true);
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/api/books', (req, res) => {
+  let skip = parseInt(req.query.skip);
+  let limit = parseInt(req.query.limit);
+  let order = req.query.order;
+
+  Book.find().skip(skip).sort({_id:order}).limit(limit).exec((err, doc) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).send(doc);
+  })
+});
+
 app.get('/api/getbook', (req, res) => {
   let id = req.query.id;
   Book.findById(id, (err, doc) => {
@@ -23,15 +34,34 @@ app.get('/api/getbook', (req, res) => {
     res.status(200).send(doc);
   });
 });
-app.post('/api/book', (req, res) => {
-  const book = new Book(req.body);
 
+app.post('/api/books', (req, res) => {
+  const book = new Book(req.body);
   book.save((err, doc) => {
     if(err) return res.status(400).send(err);
     res.status(200).json({
       post: true,
       bookId: doc._id
     })
+  })
+});
+
+app.post('/api/book_update', (req, res) => {
+  Book.findByIdAndUpdate(req.body._id, req.body, { new: true}, (err, doc) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).json({
+      success: true,
+      doc
+    });
+  });
+});
+
+app.delete('/api/book_delete', (req, res) => {
+  let id = req.query.id;
+
+  Book.findByIdAndRemove(id, (err) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).json(true);
   })
 });
 
